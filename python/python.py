ffe5d62fb7bc692827b56d99d0bf260ba8a4a2c5
Jppereira-alt <jppereirazc@gmail.com>
2026-02-23 21:45:02 -0300

    Update python.py

--- python/python.py
+++
@@ -1,8 +0,0 @@
-import math
-print( "calcule dois pontos sobre raizes quadradas")
-R1 = float(input("Digite o valor de R1: "))
-R2 = float(input("Digite o valor de R2: "))
-raiz = int(math.sqrt(R1))
-raiz2 = int(math.sqrt(R2))
-print("A raiz quadrada de R1 é: ", raiz)
-print("A raiz quadrada de R2 é: ", raiz2)
