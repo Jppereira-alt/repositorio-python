67fb3bd8370411701c994f2956cacc51764f4c39
Jppereira-alt <jppereirazc@gmail.com>
2026-02-23 22:37:51 -0300

    sorteio random.choice

--- python/python.py
+++
@@ -1,9 +0,0 @@
-import random
-
-n1=input("digite o nome de quem sera sorteado: ")
-n2 = input ("digite outro nome: ")
-n3 = input("digite mais outro nome:")
-n4 = input("digite mais um: ")
-lista = [n1, n2, n3, n4]
-num = str(random.choice(lista))
-print ("a pessoa sorteada foi: ", num,)
